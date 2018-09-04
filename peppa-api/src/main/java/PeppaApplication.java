89fa0b4b916c82df6ea6430799281ed055fa799e
unknown <qixin@tuhu.ad>
2018-09-04 19:20:05 +0800

    new

---
+++ peppa-api/src/main/java/PeppaApplication.java
@@ -0,0 +1,17 @@
+/* Copyright 2018 tuhu.cn All right reserved. This software is the
+ * confidential and proprietary information of tuhu.cn ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with Tuhu.cn
+ */
+
+/**
+ * @Auther: qixin
+ * @Date: 2018/09/04 17:53
+ */
+
+
+public class PeppaApplication {
+
+
+}
